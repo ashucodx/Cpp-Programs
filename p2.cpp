8a27f70238181727256c9ec9f176495b5d631e4f
Asheesh Yadav <letscodx@gmail.com>
2024-02-10 06:40:38 +0530

    third commit in c++

---
+++ p2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, sum;
+    cout<<"enter a: "<<a<<endl;
+    cout<<"enter b: "<<b<<endl;
+    cout<<"sum = "<<a+b;
+    return 0;
+}
